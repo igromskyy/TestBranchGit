908cbaaaa18c0bba255083a945feb9598fc6715f
IvanGromskyy <igromskyy@gmail.com>
2013-10-30 22:21:02 +0200

    Create Project

---
+++ TestBranchGit/src/pac1/Class2.java
@@ -0,0 +1,8 @@
+package pac1;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ TestBranchGit/src/pac2/Class3.java
@@ -0,0 +1,8 @@
+package pac2;
+
+public class Class3 {
+
+	public static void main(String[] args) {
+	}
+
+}
