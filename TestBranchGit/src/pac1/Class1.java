ca7e4a2e21350441a5b8d344448108ae69fc001f
iyst-01 <iyst-01@yandex.ru>
2013-10-31 20:54:06 +0200

    sdf

--- TestBranchGit/src/pac1/Class1.java
+++ TestBranchGit/src/pac1/Class1.java
@@ -3,12 +3,8 @@ package pac1;
 public class Class1 {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("Add new line6");
-=======
-		System.out.println("Add new line1");
->>>>>>> branch 'master' of https://github.com/igromskyy/TestBranchGit.git
-		
-	}
+}
 
 }
